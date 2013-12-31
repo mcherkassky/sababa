817bcf030ced23707a3d79fa9cd853a667c4e55a
mcherkassky <mcherkassky@gmail.com>
2013-12-31 13:19:53 +0200

    languages

--- sababa/rank/translate.py
+++ sababa/rank/translate.py
@@ -5,7 +5,13 @@ import goslate
 gs = goslate.Goslate()
 
 languages = {'Hebrew': 'iw',
-             'English': 'en'}
+             'English': 'en',
+             'French': 'fr',
+             'Spanish': 'sp',
+             'Mandarin': 'zh',
+             'Japanese': 'ja',
+             'Italian': 'it',
+             'German': 'de'}
 
 def translate(text, language):
     return gs.translate(text, languages[language])
